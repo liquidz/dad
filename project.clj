64be0f68c22e2b24ec666084c005956c6ae3d25b
liquidz <liquidz.uo@gmail.com>
2019-09-16 08:03:48 +0900

    Add license file

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject trattoria
   #=(clojure.string/trim #=(slurp "resources/version.txt"))
-  :description "FIXME"
+  :description "Careless configuration management tool"
   :url "https://github.com/liquidz/trattoria"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
   :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [borkdude/sci "0.0.9"]
