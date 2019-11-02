d5cf16938e3ed28ddc2dac2d78558696df85612d
liquidz <liquidz.uo@gmail.com>
2019-11-03 00:04:33 +0900

    Add aero dependency

--- project.clj
+++ project.clj
@@ -5,15 +5,18 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :deploy-repositories [["releases" :clojars]]
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [borkdude/sci "0.0.10"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.cli "0.4.2"]
+
+                 [aero "1.1.3"]
+                 [borkdude/sci "0.0.10"]
                  [camel-snake-kebab "0.4.0"]
                  [metosin/malli "0.0.1-SNAPSHOT"]]
 
   :main ^{:skip-aot true} oton.core
   :profiles
-  {:uberjar {:aot [oton.core]
+  {:dev {:resource-paths ["test/resources"]}
+   :uberjar {:aot [oton.core]
              :prep-tasks ["compile"]
              :uberjar-name "oton.jar"}}
   )
