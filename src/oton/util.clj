75be62cbd13108eed675d85e8642d27a12634408
liquidz <liquidz.uo@gmail.com>
2019-11-03 00:07:35 +0900

    Update oton.util

--- src/oton/util.clj
+++ src/oton/util.clj
@@ -1,4 +1,5 @@
-(ns oton.util)
+(ns oton.util
+  (:require [clojure.string :as str]))
 
 (defn distinct-by [f coll]
   (loop [[first-elm & rest-elms] coll
@@ -10,3 +11,13 @@
         (if (contains? ids id)
           (recur rest-elms ids result)
           (recur rest-elms (conj ids id) (conj result first-elm)))))))
+
+(defn ensure-seq [x]
+  (cond-> x
+    (not (sequential? x)) vector))
+
+(defn expand-map-to-str [s m]
+  (reduce-kv
+   (fn [res k v]
+     (str/replace res (str "%" (name k) "%") (str v)))
+   s m))
--- test/oton/util_test.clj
+++ test/oton/util_test.clj
@@ -14,3 +14,16 @@
 
     (t/is (= test-data
              (sut/distinct-by :b test-data)))))
+
+(t/deftest ensure-seq-test
+  (t/is (= [1] (sut/ensure-seq 1)))
+  (t/is (= [1] (sut/ensure-seq [1])))
+  (t/is (= [nil] (sut/ensure-seq nil))))
+
+(t/deftest expand-map-to-str-test
+  (let [m {:foo "bar" :bar "baz"}]
+    (t/are [expected input] (= expected (sut/expand-map-to-str input m))
+      "hello bar",       "hello %foo%"
+      "hello bar baz",   "hello %foo% %bar%"
+      "hello bar bar",   "hello %foo% %foo%"
+      "hello %unknown%", "hello %unknown%")))
