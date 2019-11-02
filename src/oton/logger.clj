39bad86b25edf6306e30aea78b210ae939b23cf8
liquidz <liquidz.uo@gmail.com>
2019-11-03 00:00:47 +0900

    Update logger

--- src/oton/logger.clj
+++ src/oton/logger.clj
@@ -2,15 +2,13 @@
   (:require [clojure.string :as str]))
 
 (def ^:dynamic *level* :info)
-(def ^:private levels (zipmap [:trace :debug :info :warn :error :fatal :silent] (range)) )
+(def ^:private levels (zipmap [:debug :info :warn :error :silent] (range)) )
 
 (defn- log [level msg more]
   (when (<= (get levels *level*) (get levels level))
     (println (str (str/upper-case (name level)) ":") msg more)))
 
-(def trace (partial log :trace))
 (def debug (partial log :debug))
 (def info (partial log :info))
 (def warn (partial log :warn))
 (def error (partial log :error))
-(def fatal (partial log :fatal))
