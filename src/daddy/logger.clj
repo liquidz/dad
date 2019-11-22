28cb1581641d75c4ac325744d440fdbae73dfb1d
liquidz <liquidz.uo@gmail.com>
2019-11-23 07:11:41 +0900

    Update logger to colorize

--- src/daddy/logger.clj
+++ src/daddy/logger.clj
@@ -3,10 +3,25 @@
 
 (def ^:dynamic *level* :info)
 (def ^:private levels (zipmap [:debug :info :warn :error :silent] (range)) )
+(def ^:private color-codes
+  {:debug 97
+   :info 32
+   :warn 35
+   :error 31
+   :silent 30})
 
-(defn- log [level msg more]
+(defn- colorize [code s]
+  (str \u001b "[" code "m" s \u001b "[m"))
+
+(defn- log [level msg & [more]]
   (when (<= (get levels *level*) (get levels level))
-    (println (str (str/upper-case (name level)) ":") msg more)))
+    (let [colorize* (partial colorize (get color-codes level :debug))]
+      (apply println
+             (cond->> [(colorize* (str/upper-case (name level)))
+                       ":"
+                       msg]
+               more (conj more))))
+    nil))
 
 (def debug (partial log :debug))
 (def info (partial log :info))
