db3d29c6612bd9d9599b594461967b724abb01ed
liquidz <liquidz.uo@gmail.com>
2019-11-29 19:26:33 +0900

    Tweak descriptions

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject daddy
   #=(clojure.string/trim #=(slurp "resources/version.txt"))
-  :description "Careless configuration management tool"
+  :description "Small configuration management tool for Clojure"
   :url "https://github.com/liquidz/daddy"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
