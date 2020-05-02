8a7a15250cdd5351c9d028f157078b908fa6c916
liquidz <liquidz.uo@gmail.com>
2020-05-03 07:06:53 +0900

    Bump dependencies

--- project.clj
+++ project.clj
@@ -6,10 +6,10 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.cli "0.4.2"]
+                 [org.clojure/tools.cli "1.0.194"]
 
-                 [aero "1.1.5"]
-                 [borkdude/sci "0.0.12"]
+                 [aero "1.1.6"]
+                 [borkdude/sci "0.0.13-alpha.18"]
                  [camel-snake-kebab "0.4.1"]
                  [metosin/malli "0.0.1-SNAPSHOT"]]
 
