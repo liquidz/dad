0209682a0b0aaae0b175ea6e22ddc6e159dc12fc
liquidz <liquidz.uo@gmail.com>
2022-03-13 18:09:44 +0900

    feat: Update pod support

--- src/dad/pod.clj
+++ src/dad/pod.clj
@@ -2,6 +2,7 @@
   (:require
    [bencode.core :as bencode]
    [clojure.edn :as edn]
+   [clojure.string :as str]
    [dad.const :as d.const]
    [dad.reader :as d.reader]
    [dad.runner :as d.runner])
@@ -9,13 +10,39 @@
    (java.io
     PushbackInputStream)))
 
+(def ^:private dryrun? (atom true))
+
 (def ^:private stdin
   (PushbackInputStream. System/in))
 
-(def ^:private describe-map
+(defn- run-tasks
+  [config tasks]
+  (if @dryrun?
+    (d.runner/dry-run-tasks config tasks)
+    (d.runner/run-tasks config tasks)))
+
+(defn- load-file*
+  [config path]
+  (let [{:keys [tasks]} (d.reader/read-tasks config (slurp path))]
+    (run-tasks config tasks)))
+
+(defn- pod-bindings
+  [config]
+  (merge d.reader/task-configs
+         d.reader/util-bindings
+         {'load-file (partial load-file* config)
+          'set-dryrun! #(reset! dryrun? %)}))
+
+(defn- describe-map
+  [config]
   {"format" "edn"
    "namespaces" [{"name" (str d.const/pod-name)
-                  "vars" (map #(hash-map "name" (str %)) (keys d.reader/task-configs))}]})
+                  "vars" (map (fn [[k v]]
+                                {"name" (str k)
+                                 "meta" (-> (meta v)
+                                            (select-keys [:name :doc :arglists])
+                                            (pr-str))})
+                              (pod-bindings config))}]})
 
 (defn- read-string*
   [^"[B" x]
@@ -31,15 +58,59 @@
   (.flush System/out))
 
 (defn- get-function
-  [msg]
-  (let [qualified-var-sym (-> (get msg "var")
-                              (read-string*)
-                              (symbol))
-        var-sym (-> qualified-var-sym
-                    (name)
-                    (symbol))]
-    (get-in d.reader/task-configs
-            [var-sym :destination])))
+  [config msg]
+  (let [var-sym (-> (get msg "var")
+                    (read-string*)
+                    (symbol))
+        var-sym (if (qualified-symbol? var-sym)
+                  (-> var-sym
+                      (name)
+                      (symbol))
+                  var-sym)]
+    (get (pod-bindings config) var-sym)))
+
+(defn- tasks? [x]
+  (and (sequential? x)
+       (every? #(contains? % :type) x)))
+
+(defn- debug
+  [& strs]
+  (spit "/tmp/neko" (str (str/join " " strs) "\n") :append true))
+
+(defn- invoke
+  [config id msg]
+  (if-let [f (get-function config msg)]
+    (try
+      (let [args (-> (get msg "args")
+                     (read-string*)
+                     (edn/read-string))
+            ret (apply f args)
+            config' (assoc-in config [:log :compact?] true)
+
+            out (when (tasks? ret)
+                  (debug "FIXME" (pr-str ret))
+                  (with-out-str (run-tasks config' ret)))
+            _ (debug "FIXME out" (pr-str out))
+            reply (cond-> {"value" (pr-str (when-not out ret))
+                           "id" id
+                           "status" ["done"]}
+                    out (assoc "out" out))]
+        (write* reply))
+      (catch clojure.lang.ExceptionInfo ex
+        (if (= ::d.reader/validation-error (some-> ex ex-data :type))
+          (let [{:keys [args errors]} (ex-data ex)
+                reply {"ex-message" (ex-message ex)
+                       "ex-data" (pr-str (ex-data ex))
+                       "id" id
+                       "out" (str "Validation error:\n"
+                                  (str/join "\n" (map #(str "- " (pr-str %)) errors))
+                                  "\n\n"
+                                  "Arguments:\n"
+                                  "- " (pr-str args))
+                       "status" ["done" "error"]}]
+            (write* reply))
+          (throw ex))))
+    (throw (ex-info (str "Var not found: " (read-string* (get msg "var"))) {}))))
 
 (defn start
   [config]
@@ -58,23 +129,12 @@
               id (or id "unknown")]
           (case op
             :describe
-            (do (write* describe-map)
+            (do (write* (describe-map config))
                 (recur))
 
             :invoke
             (do (try
-                  (if-let [f (get-function msg)]
-                    (let [args (-> (get msg "args")
-                                   (read-string*)
-                                   (edn/read-string))
-                          ret (apply f args)
-                          out (with-out-str (d.runner/dry-run-tasks config [ret]))
-                          reply {"value" (pr-str nil)
-                                 "out" out
-                                 "id" id
-                                 "status" ["done"]}]
-                      (write* reply))
-                    (throw (ex-info (str "Var not found: " (get msg "var")) {})))
+                  (invoke config id msg)
                   (catch Throwable ex
                     (let [reply {"ex-message" (ex-message ex)
                                  "ex-data" (pr-str (assoc (ex-data ex)
