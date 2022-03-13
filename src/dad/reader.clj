d099112b37e20f960c7959eae1489df66354198d
liquidz <liquidz.uo@gmail.com>
2022-03-13 17:30:22 +0900

    refactor: Update to validate with malli/schema

--- src/dad/reader.clj
+++ src/dad/reader.clj
@@ -1,6 +1,5 @@
 (ns dad.reader
   (:require
-   [camel-snake-kebab.core :as csk]
    [clojure.java.io :as io]
    [clojure.string :as str]
    [dad.const :as d.const]
@@ -14,32 +13,69 @@
 
 (declare read-tasks)
 
-(def task-configs
-  {'directory, {:destination #'d.r.impl/directory
-                :resource-name-key :path}
-   'execute,   {:destination #'d.r.impl/execute
-                :resource-name-key :command}
-   'file,      {:destination #'d.r.impl/file
-                :resource-name-key :path}
-   'git,       {:destination #'d.r.impl/git
-                :resource-name-key :path}
-   'download,  {:destination #'d.r.impl/download
-                :resource-name-key :path}
-   'link,      {:destination #'d.r.impl/link
-                :resource-name-key :path}
-   'package,   {:destination #'d.r.impl/package
-                :resource-name-key :name}
-   'template,  {:destination #'d.r.impl/template
-                :resource-name-key :path}})
+(defn- task-id
+  [m]
+  (->> m
+       (sort-by (comp str first)) ; sort by map key
+       (map (comp str second)) ; extract map val
+       (str/join "")))
 
-(def ^:private util-bindings
-  {'doc          "DUMMY: associated at `read-tasks` formally"
-   'env          #(get (System/getenv) (csk/->SCREAMING_SNAKE_CASE_STRING %))
-   'file-exists? #(some-> % io/file (.exists))
+(defn- ensure-task-list
+  [x]
+  (->> (d.util/ensure-seq x)
+       (remove nil?)
+       (map #(assoc % :id (task-id %)))))
+
+(defn- extract-function-input-schema
+  [v]
+  (let [m (meta v)]
+    (-> (get m :schema
+             (get m :malli/schema))
+        (m/schema)
+        (m/-function-info)
+        (get :input))))
+
+(defn wrap-task
+  [function-var]
+  (with-meta (fn [& args]
+               (let [schema (some-> function-var
+                                    (extract-function-input-schema))]
+                 (when-not schema
+                   (throw (ex-info "no validation schema error" {:var function-var})))
+                 (if-let [err (some-> schema
+                                      (m/explain args)
+                                      (me/humanize))]
+                   (throw (ex-info "validation error" {:type ::validation-error
+                                                       :args args
+                                                       :errors err}))
+                   (ensure-task-list
+                    (apply function-var args)))))
+    (meta function-var)))
+
+
+(def task-configs
+  (reduce
+   (fn [accm function-var]
+     (let [m (meta function-var)]
+       (assoc accm (:name m) (wrap-task function-var))))
+   {}
+   [#'d.r.impl/directory
+    #'d.r.impl/execute
+    #'d.r.impl/file
+    #'d.r.impl/git
+    #'d.r.impl/download
+    #'d.r.impl/link
+    #'d.r.impl/package
+    #'d.r.impl/template]))
+
+(def util-bindings
+  {'file-exists? #(some-> % io/file (.exists))
    'os-type      (fn [] (name (d.os/os-type)))
    'render       #(d.util/expand-map-to-str %1 %2 "{{" "}}")
-   'help         "DUMMY: associated at `read-tasks` formally"
    'load-file    "DUMMY: associated at `read-tasks` formally"})
+
+(def ^:private system-binding
+  {'getenv #(System/getenv %)})
 
 (defn- extract-doc
   [resource-name]
@@ -68,38 +104,6 @@
      (println s)
      (println (str "Unknown name: " resource-name)))))
 
-(defn- validate
-  [value schema]
-  (if-let [err (some-> schema
-                       (m/explain value)
-                       me/humanize)]
-    (throw (ex-info "validation error" err))
-    value))
-
-(defn- dispatch*
-  [task-config & args]
-  (let [{:keys [schema destination resource-name-key]} task-config
-        [resource-name m] (cond->> args
-                            (some-> args first map?) (cons nil))
-        arg-map (when (or (nil? m) (map? m))
-                  (cond-> (or m {})
-                    resource-name (assoc resource-name-key resource-name)))
-        arg-map (validate arg-map schema)]
-    (destination arg-map)))
-
-(defn- task-id
-  [m]
-  (->> m
-       (sort-by (comp str first)) ; sort by map key
-       (map (comp str second)) ; extract map val
-       (str/join "")))
-
-(defn- ensure-task-list
-  [x]
-  (->> (d.util/ensure-seq x)
-       (remove nil?)
-       (map #(assoc % :id (task-id %)))))
-
 (defn load-file*
   [ctx path]
   (-> path
@@ -107,27 +111,20 @@
       (sci/eval-string ctx)))
 
 (defn- build-task-bindings
-  [tasks-atom config]
+  [tasks-atom]
   (letfn [(add-tasks
             [tasks]
-            (doseq [task (ensure-task-list tasks)]
+            (doseq [task tasks]
               (d.log/debug "Read task" task)
               (swap! tasks-atom conj task)))]
     (reduce-kv
-     (fn [res k v]
-       (let [schema (get-in config [:schema k])
-             task-config (assoc v :schema schema)
-             doc (-> k name extract-doc)]
-         (when-not schema
-           (throw (ex-info "no validation schema error" {:name k})))
-         (assoc res k (with-meta (comp add-tasks (partial dispatch* task-config))
-                        (-> task-config :destination meta
-                            (cond-> doc (assoc :doc doc)))))))
+     (fn [accm fname f]
+       (assoc accm fname (with-meta (comp add-tasks f) (meta f))))
      {} task-configs)))
 
 (defn build-bindings
-  [tasks config]
-  (-> (build-task-bindings tasks config)
+  [tasks]
+  (-> (build-task-bindings tasks)
       (merge util-bindings)
       (assoc 'doc doc
              ;; Alias for easy to remember
@@ -138,7 +135,8 @@
   (let [tasks (atom [])
         env (:env config (atom {}))
         ctx {:namespaces {'babashka.pods {'load-pod (constantly nil)}
-                          d.const/pod-name (build-bindings tasks config)}
+                          d.const/pod-name (build-bindings tasks)
+                          'System system-binding}
              :env env}
         ctx (update ctx :bindings assoc
                     'load-file (partial load-file* ctx))
@@ -146,25 +144,3 @@
               (sci/eval-string code-str ctx))]
     {:res res
      :tasks (d.util/distinct-by :id @tasks)}))
-
-(comment
-  (let [sample "
-
-        (ns my-project.core
-          (:require [babashka.pods :as pods]))
-
-        (pods/load-pod \"pod-liquidz-dad\")
-
-        (require '[pod.liquidz.dad :as dad])
-
-        (println \"neko\")
-        (println (System/getenv \"HOME\"))
-        (dad/directory {:path \"inu\"})
-        (dad/directory {:path \"inu\"})
-
-               "]
-    (try
-      (:tasks (read-tasks (dad.config/read-config) sample))
-      (catch clojure.lang.ExceptionInfo ex
-        (println (ex-message ex) (ex-data ex))))))
-
