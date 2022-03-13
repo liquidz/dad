4d7c3d7f08a0becf787923689a459a44deff08b6
liquidz <liquidz.uo@gmail.com>
2022-03-14 06:50:17 +0900

    test: Fix test tasks

--- test/resources/test_task/loaded_tasks.clj
+++ test/resources/test_task/loaded_tasks.clj
@@ -1,10 +1,12 @@
-(package "sl" {:action :uninstall}) ;; should be skipped
-(package "sl")
-(package "cowsay" {:action :uninstall})
+(require '[pod.liquidz.dad :as dad])
+
+(dad/package {:name "sl" :action :uninstall}) ;; should be skipped
+(dad/package {:name "sl"})
+(dad/package {:name "cowsay" :action :uninstall})
 
 (let [opt {:path (base-dir "tmpl")
            :source "template.tmpl"
            :variables {:foo "bar" :bar "baz"}
            :mode "644"}]
-  (template opt)
-  (template (assoc opt :mode "755")))
+  (dad/template opt)
+  (dad/template (assoc opt :mode "755")))
--- test/resources/test_task/tasks.clj
+++ test/resources/test_task/tasks.clj
@@ -1,29 +1,33 @@
 #!/usr/bin/env dad --no-color --dry-run
+(ns foo
+  (:require
+   [pod.liquidz.dad :as dad]))
 
 (defn base-dir [x]
   (dad/render "/tmp/dad_test/{{x}}" {:x  x}))
 
-(directory (base-dir "post") {:mode "755"})
-(directory (base-dir "pre/a") {:action :delete})
-(directory (base-dir "post") {:mode "744"})
+(dad/directory {:path (base-dir "post") :mode "755"})
+(dad/directory {:path (base-dir "pre/a") :action :delete})
+(dad/directory {:path (base-dir "post") :mode "744"})
 
-(download {:path (base-dir "project.clj")
-           :url "https://raw.githubusercontent.com/liquidz/dad/master/project.clj"
-           :mode "755"})
+(dad/download {:path (base-dir "project.clj")
+               :url "https://raw.githubusercontent.com/liquidz/dad/master/project.clj"
+               :mode "755"})
 
-(file (base-dir "foo"))
-(file (base-dir "pre/dummy") {:action :delete})
+(dad/file {:path (base-dir "foo")})
+(dad/file {:path (base-dir "pre/dummy") :action :delete})
 
-(git {:url "https://github.com/liquidz/dad"
-      :path (base-dir "dad")})
+(dad/git {:url "https://github.com/liquidz/dad"
+          :path (base-dir "dad")})
 
-(execute {:command "touch hello"
-          :cwd (base-dir "")})
+(dad/execute {:command "touch hello"
+              :cwd (base-dir "")})
 
-(execute {:command "touch error"
-          :cwd (base-dir "")
-          :pre-not "test -e hello"})
+(dad/execute {:command "touch error"
+              :cwd (base-dir "")
+              :pre-not "test -e hello"})
 
-(link (base-dir "world") {:to (base-dir "hello")})
+(dad/link {:path (base-dir "world")
+           :to (base-dir "hello")})
 
-(load-file "loaded_tasks.clj")
+(dad/load-file "loaded_tasks.clj")
